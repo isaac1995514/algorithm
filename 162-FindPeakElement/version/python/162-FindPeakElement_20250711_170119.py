1cc810a1200ff674043eea92bcb1caf3e92ef33e
Isaac K Leong <isaackleong514@gmail.com>
2025-07-11 17:01:20 -0400

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/version/python/162-FindPeakElement_20250711_170119.py
@@ -0,0 +1,17 @@
+# Last updated: 7/11/2025, 5:01:19 PM
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > (nums[m-1] if m > 0 else float('-inf')) and nums[m] > (nums[m+1] if m < len(nums) - 1 else float('-inf')):
+                return m
+            elif nums[m] < nums[m+1]:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return l
+
+                
